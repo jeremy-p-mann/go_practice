ecbe99768f65d5f345933587b01dcb6b33d620b8
Jeremy Mann <jmann227@gmail.com>
2024-02-01 19:11:27 -0800

    play more with pointers

--- pointers.go
+++ pointers.go
@@ -1,22 +1,28 @@
 package main
 
-import "fmt" 
+import "fmt"
 
-func zeroval (ival int) {
-    ival = 0
+func zeroval(ival int) {
+	ival = 0
 }
 
 func zeroptr(iptr *int) {
-    *iptr = 0
+	*iptr = 0
 }
 
 func main() {
 
-    i := 1
-    fmt.Println("initial: ", i) 
-    zeroval(i)
-    fmt.Println("zeroval: ", i) 
-    zeroptr(&i)
-    fmt.Println("zeroptr: ", i) 
-    fmt.Println("pointer: ", &i) 
+	i := 1
+	fmt.Println("initial: ", i)
+	zeroval(i)
+	fmt.Println("zeroval: ", i)
+	zeroptr(&i)
+	fmt.Println("zeroptr: ", i)
+	fmt.Println("pointer: ", &i)
+
+	var j int
+	k := &j
+	if k == nil {
+		fmt.Println("hi")
+	}
 }
